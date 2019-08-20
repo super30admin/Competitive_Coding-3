a40971ec49cf46920fb552faf327c245c912f7a2
Prakash <prakasa@rose02yd0b8jgh8.es.ad.adp.com>
2019-08-20 14:48:07 -0700

    Updated Pascal triangle Solution without Formula

--- Pascal's Triangle II.java
+++ Pascal's Triangle II.java
@@ -20,6 +20,30 @@ Could you optimize your algorithm to use only O(k) extra space?
 // T: O(k)
 // S: O(k)
 
+// Solution without Formula
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        
+        List<Integer> result = new ArrayList<Integer>();
+        
+        for(int i = 0; i <= rowIndex; i++){
+            
+            result.add(1);
+            
+            for(int j = i - 1; j > 0; j--){
+                
+                result.set(j, result.get(j - 1) + result.get(j));
+            }
+        }
+        
+        return result;
+    }
+}
+
+
+// Solution with formula
+
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         
