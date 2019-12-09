0ac6699c7254277460a542cf64efe00e968f6238
devashishnyati <devashishnyati@gmail.com>
2019-12-08 22:33:44 -0800

    fix

--- countPairs.py
+++ countPairs.py
@@ -7,23 +7,25 @@ class Solution(object):
             else:
                 lookup[num] = 1
         result = 0
-        
-        setNums = list(set(nums))
-        for i in range(len(setNums)):
+
+        # setNums = list(set(nums))
+        for i in range(len(nums)):
+            
             if k == 0:
-                if lookup[setNums[i]] > 1:
-                    lookup[setNums[i]] = 0
+                if lookup[nums[i]] > 1:
+                    lookup[nums[i]] = 0
                     result += 1
             else:
-                if setNums[i] - k in lookup and lookup[setNums[i - k]] > 0:
-                    lookup[setNums[i - k]] = 0
+                if nums[i] - k in lookup and lookup[nums[i] - k] > 0:
+                    lookup[nums[i] - k] = 0
                     result += 1
+            print(nums[i], lookup, result)
         return result
 
 obj = Solution()
-nums = [1, 2, 3, 4, 5, 3]
-k = 1
+nums = [1, 2, 3, 4, 5]
+k = -1
 print(obj.countPairs(nums, k))
 nums = [1, 3, 1, 5, 4, 8]
 k = 0
-print(obj.countPairs(nums, k))
+# print(obj.countPairs(nums, k))
