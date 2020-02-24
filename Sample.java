08b6d5010614fae9e961f58869285a9934d5b63a
priyanka1012-singhal <priyanka.singhal@sjsu.edu>
2020-02-24 11:47:51 -0800

    pascal triangle

--- Sample.java
+++ Sample.java
@@ -5,3 +5,31 @@
 
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    
+    //  Time complexity - O(numRows^2)
+    //  Space complexity - O(numRows) for curr list
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(numRows == 0)
+            return res;
+        
+        for(int i = 0; i < numRows; i++){
+            List<Integer> curr = new ArrayList<>(); 
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i){
+                    curr.add(1);
+                }else{
+                    int left = res.get(i - 1).get(j - 1);
+                    int right = res.get(i - 1).get(j);
+                    curr.add(left + right);
+                }
+            } 
+            res.add(curr);
+            
+        }
+        
+        return res;
+    }
+}
