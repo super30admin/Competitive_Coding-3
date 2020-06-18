7734f7bbd416cbf428ec7b30d53019eebbba3952
Shantanu609 <shantanu_shinde_@mail.fresnostate.edu>
2020-06-17 18:06:06 -0700

    Problem2, done using HashMap, based on review comments

--- Problem2.py
+++ Problem2.py
@@ -1,27 +1,47 @@
-# Time Complexity : O(nlogn) + O(n) 
+# Time Complexity : O(nlogn)
 # Space Complexity :O(1)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 # Your code here along with comments explaining your approach
 class Solution:
+    # def findPairs(self,nums,k):
+    #     if not nums:
+    #         return 0
+    #     nums.sort()
+    #     low = 0 
+    #     high = 1 
+    #     count = 0 
+    #     while low < len(nums) and high < len(nums):
+    #         if low == high or nums[low] + k > nums[high]:
+    #             high += 1 
+    #         elif nums[low] + k < nums[high]:
+    #             low += 1 
+    #         else:
+    #             count += 1 
+    #             low += 1 
+    #             while low < len(nums) and nums[low] == nums[low-1]:
+    #                 low += 1 
+    #     return count
+    
     def findPairs(self,nums,k):
         if not nums:
-            return 0
-        nums.sort()
-        low = 0 
-        high = 1 
+            return 0 
+        if k < 0 :
+            return 0 
+        d = {}
         count = 0 
-        while low < len(nums) and high < len(nums):
-            if low == high or nums[low] + k > nums[high]:
-                high += 1 
-            elif nums[low] + k < nums[high]:
-                low += 1 
+        for num in nums:
+            if num not in d :
+                if num + k in d:
+                    count += 1 
+                if num - k in d:
+                    count += 1 
+                d[num] = 1 
             else:
-                count += 1 
-                low += 1 
-                while low < len(nums) and nums[low] == nums[low-1]:
-                    low += 1 
-        return count
+                if k == 0 and d[num] == 1 :
+                    count += 1 
+                d[num] += 1
+        return count 
         
 if __name__ == "__main__":
     s = Solution()
