dc327ac38a4843fbf04d7114593ba12025cba477
Arpit Sharma <arpit317@gmail.com>
2020-07-26 16:52:14 -0700

    Create Pascal'sTriangle.java

---
+++ Pascal'sTriangle.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(numRows == 0) return result;
+        
+        for(int i = 0; i < numRows; i++){
+            List<Integer> list = new ArrayList<>();
+            for(int j = 0; j <= i; j++){
+                if(j==0 || j == i) list.add(1);
+                
+                else{
+                    list.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
