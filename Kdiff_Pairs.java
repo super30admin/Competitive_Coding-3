440069e9e3e1b45e9e014035024b21e24c75adc6
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-07-27 10:08:02 -0500

    K-diff Pairs in an Array

---
+++ Kdiff_Pairs.java
@@ -0,0 +1,65 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+
+
+import java.util.*;
+
+public class Kdiff_Pairs {
+	public int findPairs(int[] nums, int k) {
+		if(nums == null || nums.length == 0)
+			return 0;
+		if (k < 0)
+			return 0;
+
+		Set<Integer> result = new HashSet<>();
+		Set<Integer> set = new HashSet<>();
+
+		for(int i = 0; i< nums.length; i++){
+			if(set.contains(nums[i]-k))
+				result.add(nums[i]-k);
+
+			if(set.contains(nums[i]+k))
+				result.add(nums[i]);
+
+			set.add(nums[i]);    
+		}
+		return result.size();   
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
+
+/**********************************************************/
+//Time Complexity : O(n logn)
+//Space Complexity : O(1)
+//Did this code successfully run on Leetcode : Yes
+
+class Solution {
+	public int findPairs(int[] nums, int k) {
+		Arrays.sort(nums);
+		int count =0;
+		int low = 0;
+		int high = 0;
+		while(low< nums.length && high < nums.length){
+			if(low == high || nums[high] - nums[low] < k)
+				high++;
+
+			else if(nums[high] - nums[low] > k)
+				low++;
+
+			else{
+				count++;
+				low++;
+				high++;
+
+				//check for unique
+				while(high < nums.length && nums[high]== nums[high-1])
+					high++;
+			}
+		}
+		return count;
+	}
+}
