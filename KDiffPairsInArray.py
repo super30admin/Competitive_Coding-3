3ff2f93849c38373d39943e9cb0c703961c2d1a3
Arpit Sharma <arpit317@gmail.com>
2020-07-27 14:04:24 -0700

    Create KDiffPairsInArray.py

---
+++ KDiffPairsInArray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        slow = 0
+        fast = 1
+        count = 0
+        nums.sort()
+        while slow < len(nums) and fast < len(nums):
+            diff = nums[fast] - nums[slow]
+            if diff == k:
+                count+=1
+                slow += 1
+                fast += 1
+                while fast < len(nums) and nums[fast]==nums[fast-1]:
+                    fast += 1
+            elif diff > k:
+                slow+=1
+            else:
+                fast+=1
+            if slow == fast:
+                fast += 1
+
+        return count
+
+
