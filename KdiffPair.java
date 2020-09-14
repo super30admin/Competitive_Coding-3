04a4b62a9989b378cf598a705d08a44c93ee3a7a
sidmal11 <39958179+sidmal11@users.noreply.github.com>
2020-09-14 19:42:53 -0400

    Kdiff pair 2 ptr

--- KdiffPair.java
+++ KdiffPair.java
@@ -1,4 +1,47 @@
-// Time Complexity :O(n) since we got through the array twice(one for hashmap,other to traverse if complement is present)
+
+// Time Complexity : O(n) since we need to see each element atleat once 
+// Space Complexity : O(1) since we can do in constant space
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+
+// Two Ptr approach
+// we first sort the array and put the start and complement both at 0
+// then we either 
+// move complement ptr to complement+1 when nums[start] +k is too strong for nums[complement] or start == complement
+// move start ptr to start+1 when nums[start] +k is not enough for nums[complement]
+// else increment ans and start.. and also if the element at complement+1 is same as complement && complement is still inside boundary skip compliment  
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        // 2 ptr approach
+        if (nums.length <= 1)
+            return 0;
+
+        int ans = 0;
+        Arrays.sort(nums);
+        int start = 0, complement = 0;
+
+        // until start or complement reaches the end of nums continue
+        while (start < nums.length - 1 && complement < nums.length - 1) {
+            if (start == complement || nums[start] + k > nums[complement])
+                complement++;
+            else if (nums[start] + k < nums[complement])
+                start++;
+            else {
+                ans++;
+                start++;
+                while (complement < nums.length - 1 && nums[complement] == nums[complement + 1])
+                    complement++;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Time Complexity :O(n) since we got through the array twice(one for
+// hashmap,other to traverse if complement is present)
 // Space Complexity :O(n) where n is the number of entries in the hashmap
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
