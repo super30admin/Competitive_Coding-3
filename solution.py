939fe9ca2f5ae65ab2c4de350bf3b94e5c3fff86
Vijay Raja <jobprepnyu@gmail.com>
2020-11-03 16:02:09 -0500

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,13 @@
+Solution:
+
+Time : O(N^2) Space : O(N^2)
+
+class Solution(object): 
+def generate(self, numRows): 
+  output = [] 
+  for row in range(1, numRows+1): 
+    new_row = [1]*row 
+    for j in range(1, row-1): 
+      new_row[j] = output[-1][j-1] + output[-1][j] 
+    output.append(new_row) 
+  return output
