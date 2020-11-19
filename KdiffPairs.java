20367472aea372de88145b120ba3f4257a8cfae8
Gahana-Kundur <gahanakundur@gmail.com>
2020-11-19 15:05:55 -0800

    Implemented CC-3 and able to run both the problems in LC

---
+++ KdiffPairs.java
@@ -0,0 +1,32 @@
+// Time Complexity : O(n^2)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+//Using brute force approach to find the diff pairs
+import java.util.Arrays;
+
+public class KdiffPairs {
+    public int findPairs(int[] nums, int k) {
+
+        Arrays.sort(nums);
+
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                if (Math.abs(nums[j] - nums[i]) == k)
+                    result++;
+            }
+        }
+
+        return result;
+    }
+}
