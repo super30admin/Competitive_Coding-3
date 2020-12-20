88620b27e04f16540140a768b02f82d6afe043d5
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-19 17:38:30 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,28 @@
 
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> generate(int numRows) {
+        res = new ArrayList<>();
+        if(numRows == 0) return res;
+        res.add(new ArrayList<>());
+        res.get(0).add(1);
+        for(int i = 1; i < numRows; ++i)
+        {
+            List<Integer> l = new ArrayList<>();
+            l.add(1);
+            for(int j = 1; j < i; ++j)
+            {
+                l.add(res.get(i-1).get(j-1) + res.get(i-1).get(j));
+            }
+            l.add(1);
+            res.add(l);
+        }
+        return res;
+    }
+}
+
+//TC : O(numRows^2)
+// SC : O(numRows^2)
