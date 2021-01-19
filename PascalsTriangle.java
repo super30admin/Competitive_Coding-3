de706b77d662d391a80a0b724455f693dd9be3b2
Abhishek Chaudhary <abhishek.chaudhary.2812@gmail.com>
2021-01-18 20:45:50 -0600

    competitive coding

---
+++ PascalsTriangle.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public List<List<Integer>> generate(int numRows) 
+    {
+        
+        List<List<Integer>> output = new ArrayList<>();
+        
+        
+        for(int i =0;i < numRows; i++){
+            
+            List<Integer> level = new ArrayList<>();
+            
+            for(int j =0; j <= i; j++)
+            {
+                
+                if(j == 0 || j ==i) level.add(1);
+                else
+                {
+                    List<Integer> previous = output.get(i-1);
+                    level.add(previous.get(j-1) + previous.get(j));
+                }
+                
+            }
+            
+            output.add(level);
+        }
+        
+        
+        
+        return output;
+        
+    }
+}
