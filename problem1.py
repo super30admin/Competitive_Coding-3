4abd12647b331c2fa9e09ed43618557eaf6b7e96
akashtanwani <77640435+akashtanwani@users.noreply.github.com>
2021-02-08 00:49:37 -0600

    steps added

--- problem1.py
+++ problem1.py
@@ -7,18 +7,18 @@
 // Your code here along with comments explaining your approach
 class Solution:
     def generate(self, row: int) -> List[List[int]]:
-        lst=[[1],[1,1]]
-        if row==0:
+        lst=[[1],[1,1]]                                         
+        if row==0:                                  //if row==0 return empty list
             return []
-        elif row==1:
+        elif row==1:                    //if row==1 return [[1]]
             return [lst[0]]
-        elif row==2:
+        elif row==2:                    //if row==2 return [[1],[1,1]]
             return lst[:2]
-        for i in range(2,row):
-            lst.append([1])
-            for j in range(0,len(lst[-2])-1):
-                lst[-1].append(lst[-2][j]+lst[-2][j+1])
-            lst[-1].append(1)
+        for i in range(2,row):                  if rows >2:
+            lst.append([1])                    append new list in lst with single element as 1    
+            for j in range(0,len(lst[-2])-1):           //traverse the second last list from lst till second last element
+                lst[-1].append(lst[-2][j]+lst[-2][j+1])        //add two numbers at a time fetched from second last list and append it in the last list
+            lst[-1].append(1)                               //append element 1 in the last list
             
         return lst
         
