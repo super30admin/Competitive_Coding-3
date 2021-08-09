13b886017d6e5e70876b3ffa1f9a6f4d23bf8eee
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-09 19:11:10 -0400

    Create Problem-2.cpp

---
+++ Problem-2.cpp
@@ -0,0 +1,58 @@
+//Time Complexity = O(N)
+//Space Complexity = O(N)
+//where N is the elements in the vector nums. 
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        int i,n,count=0;
+        map<int,int> m;
+        set<vector<int>> s;
+        for(i=0;i<nums.size();i++)
+        {
+            n=s.size();
+            if(m.find(nums[i]+k)!=m.end())
+            {
+                s.insert({nums[i],nums[i]+k});
+                if(n!=s.size())
+                {
+                    cout<<nums[i]+k<<" "<<nums[i]<<endl;
+                    count++;
+                }
+            }
+            n=s.size();
+            if(m.find(nums[i]-k)!=m.end())
+            {
+                s.insert({nums[i]-k,nums[i]});
+                if(n!=s.size())
+                {
+                    cout<<nums[i]-k<<" "<<nums[i]<<endl;
+                    count++;
+                }
+            }
+            m[nums[i]]=0;
+        }
+        return count;
+    }
+};
+
+//Time Complexity = O(N)
+//Space Complexity = O(1)
+//where N is the elements in the vector nums. 
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        int count=0,slow=0,fast=0, n=nums.size();
+        sort(nums.begin(),nums.end());
+        while(slow<n && fast<n)
+        {
+            if(slow==fast || nums[fast]-nums[slow]<k) fast++;
+            else if(nums[fast]-nums[slow]>k) slow++;
+            else { count++;
+                  slow++;
+                  fast++;
+            
+            while(fast<n && nums[fast]==nums[fast-1]) fast++;}
+        }
+        return count;
+    }
+};
