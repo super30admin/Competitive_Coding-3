d39841414367e99863fa95c1edf0109256638adc
Neha Poonia <69773692+NehaPoonia@users.noreply.github.com>
2021-08-10 09:25:05 +0530

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -5,13 +5,11 @@ import collections
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
         #take count of each element of nums
-        d=list(set(nums))
-        
+        d=collections.Counter(nums)
         #check for d[i]+k exist in d and  and k>0  
         res=0
-        for i in range(0,len(d)):
-            for j in range(i+1,len(d)):
-                if abs(d[i]-d[j])==k:
-                    res=res+1
+        for i in d:
+            if (i+k in d and k>0) or (k==0 and d[i]>1):
+                res=res+1
         return res
         
