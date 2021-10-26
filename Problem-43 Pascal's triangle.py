dbb5bd47a4ff8888b2fc6b2bd8ca292a13e7b67b
Adhish Shrivastava <adhish70@gmail.com>
2021-10-26 09:28:43 -0400

    Initial commit

---
+++ Problem-43 Pascal's triangle.py
@@ -0,0 +1,11 @@
+# 118. Pascal's Triangle
+# https://leetcode.com/problems/pascals-triangle/
+
+# Time complexiety: 
+# Space complexiety: 
+
+# logic: 
+
+class Solution:
+    def generate(self, numRows):
+        pass
---
+++ Problem-44 K-diff Pairs in an Array.py
@@ -0,0 +1,11 @@
+# 532. K-diff Pairs in an Array
+# https://leetcode.com/problems/k-diff-pairs-in-an-array/
+
+# Time complexiety: 
+# Space complexiety: 
+
+# logic: 
+
+class Solution:
+    def findPairs(self, nums, k: int):
+        pass
