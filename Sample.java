53909ad6a9ac269fc1e2fd3956466c1947370d22
vinitvilasgirkar <girkar001@gmail.com>
2021-11-09 12:22:34 -0500

    Completed - Please Review

--- Sample.java
+++ Sample.java
@@ -1,7 +1,38 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity : O(NlogN)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        if(nums == null && nums.length == 0)
+            return 0;
+        if(k<0)
+            return 0;
+        Arrays.sort(nums);
+        int start = 0;
+        int end = 1;
+        int n = nums.length;
+        int x = Integer.MIN_VALUE;
+        int y = Integer.MIN_VALUE;
+        int count = 0;
+        while(end<n){
+            int diff = nums[end] - nums[start];
+            if(diff == k && start != end && (x!=nums[start] && y!=nums[end])){
+                count++;
+                x = nums[start];
+                y = nums[end];
+                end++;
+
+            }
+            else if(diff>k)
+                start++;
+            else
+                end++;
+        }
+        return count;
+
+    }
+}
 
 
 // Your code here along with comments explaining your approach
