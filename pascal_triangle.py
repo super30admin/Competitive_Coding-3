756708b667906ca0ddede215938a6a772efc8f27
ravirammamidi <tatsuya.s.mknr@gmail.com>
2022-01-10 22:21:26 -0700

    completed interview question

---
+++ pascal_triangle.py
@@ -0,0 +1,27 @@
+def generate_pascals_triange(n):
+    res = []
+    res.append([1])
+
+    if n == 1:
+        print( res)
+
+    res.append([1,1])
+    if n==2:
+        print( res)
+
+    for i in range(2,n):
+        temp = [0]*(i + 1)
+        for j in range(0, i+1):
+            if j == 0: #
+                temp[j] = res[i-1][j]
+            elif j == i: #
+                temp[j] = 1
+            else:
+                temp[j] = res[i-1][j] + res[i-1][j-1]
+        res.append(temp)
+    return res
+
+if __name__=="__main__":
+    n = 4
+    print(generate_pascals_triange(n))
+    
