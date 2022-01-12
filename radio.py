d829fd9cefc0e337b3b56158219092e875277662
Patrick <patrick.trias@gmail.com>
2022-01-11 16:42:37 -0800

    Create radio.py

---
+++ radio.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hackerlandRadioTransmitters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY x
+#  2. INTEGER k
+#
+
+def hackerlandRadioTransmitters(x, k):
+    # Write your code here
+    x.sort()
+    minimum = x[0]
+    maximum = x[len(x)-1]
+    
+    range = k * 2
+    
+    diff = maximum - minimum
+    return math.ceil(diff/range)
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    x = list(map(int, input().rstrip().split()))
+
+    result = hackerlandRadioTransmitters(x, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
