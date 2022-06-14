688941021e352d9aeeed16f7a0cfaf6c11a3c10c
STSC <STSC@LENOVO-127795.ad.csueastbay.edu>
2022-06-13 21:59:07 -0700

    Completed

---
+++ pascalTriangle.java
@@ -0,0 +1,24 @@
+// Time Complexity :O(n^2)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :Yes was difficult for me to understand
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for(int i=0; i<numRows; i++){
+             List<Integer> row = new ArrayList<>();
+            for(int j=0; j<=i; j++){
+                if(j==0 || j==i) row.add(1);
+                else {
+                    int left = result.get(i-1).get(j-1);
+                    int right = result.get(i-1).get(j);
+                    row.add(left + right);
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
