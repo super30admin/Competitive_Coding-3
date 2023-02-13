0ecdd2334a26a967d36c2ad1a46f4b22134e3382
Akhil Reddy Duggasani <duggasan@usc.edu>
2023-02-12 19:38:34 -0800

    Completed Competitive Coding-3: Problem 1 & 2 - Please review

--- Sample.java
+++ Sample.java
@@ -1,7 +1,37 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+import java.util.ArrayList;
+import java.util.List;
 
+// Time Complexity : O(N^2)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 // Your code here along with comments explaining your approach
+
+/**
+ * Implementation problem. Put 1 in the first list.
+ * from next row, fetch the previous row and add the
+ * jth and j-1th elements from 1 to i-1th index. for 0 and
+ * ith index, put 1 in the current row and push that to ans list.
+ * return ans list.
+ *
+ */
+class Solution {
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> ans = new ArrayList<>();
+		List<Integer> first = new ArrayList<>();
+		first.add(1);
+		ans.add(first);
+		for (int i = 1; i < numRows; i++) {
+			List<Integer> res = new ArrayList<>();
+			List<Integer> prev = ans.get(i - 1);
+			res.add(1);
+			for (int j = 1; j < i; j++) {
+				res.add(prev.get(j - 1) + prev.get(j));
+			}
+			res.add(1);
+			ans.add(res);
+		}
+		return ans;
+	}
+}
