e09a9f2a1af6fdf6860de63ae29f9bc972b6e29c
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-04-13 17:25:24 -0700

    Update PascalsTriangle.java

--- src/PascalsTriangle.java
+++ src/PascalsTriangle.java
@@ -1,2 +1,35 @@
 // https://leetcode.com/problems/pascals-triangle/description/
 
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> previousRow = new ArrayList<>();
+        List<Integer> currentRow = new ArrayList<>();
+
+        currentRow = new ArrayList<>(Arrays.asList(1)); 
+        result.add(currentRow);                                                      // adding [1] in result
+        if(numRows == 1)                                       
+            return result;
+
+        currentRow = new ArrayList<>(Arrays.asList(1,1));
+        result.add(currentRow);                                                      // adding [1,1] in result
+        if(numRows == 2)
+            return result;
+
+        previousRow = currentRow;                                                    // updating previous row
+
+        for(int i=2; i<numRows; i++) {
+            currentRow = new ArrayList<>(Arrays.asList(1,1));                        // starting with [1,1] for each row
+            for(int j=1; j<i; j++) {
+                int value = previousRow.get(j-1) + previousRow.get(j);
+                currentRow.add(j, value);                                            // updating current row at indexes
+            }
+            previousRow = currentRow;                                                // updating previous row
+            result.add(currentRow);                                                  // adding current row in result
+        }
+
+        return result;
+
+    }
+}
